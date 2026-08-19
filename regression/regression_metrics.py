7db9c378d5bd59d7044b6c6ac048d102e264e100
tdoan <thanhdoan252024@gmail.com>
2026-08-19 19:46:02 +0700

    Remove phần thừa

--- regression/regression_metrics.py
+++ regression/regression_metrics.py
@@ -1,16 +1,1 @@
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-def calculate_regression_metrics(y_true, y_pred):
-    return {
-        "MSE": mean_squared_error(y_true, y_pred),
-        "MAE": mean_absolute_error(y_true, y_pred),
-        "R2": r2_score(y_true, y_pred),
-    }
-
-def print_regression_metrics(y_true, y_pred):
-    metrics = calculate_regression_metrics(y_true, y_pred)
-    print("===== KẾT QUẢ ĐÁNH GIÁ =====")
-    print(f"MSE : {metrics['MSE']:.4f}")
-    print(f"MAE : {metrics['MAE']:.4f}")
-    print(f"R2  : {metrics['R2']:.4f}")
-    return metrics
